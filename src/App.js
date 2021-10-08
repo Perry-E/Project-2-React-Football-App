4ebe269d7ca92d8627b175bc64f4d739450d5d7a
Perry-E <perryeber@gmail.com>
2021-10-08 12:36:30 +0800

    Test deployment with API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import React from "react"
-import Main from "./Main"
+import React, { useState, useEffect } from "react";
+import Main from "./Main";
 
 function App() {
-  const API_Key = process.env.REACT_APP_API_FOOTBALL_KEY
-  console.log(API_Key)
+  const API_Key = process.env.REACT_APP_API_FOOTBALL_KEY;
+  const [status, setStatus] = useState("idle");
+  const [leagueTable, setLeagueTable] = useState();
+
+  const LeagueTable = () => {
+
+    useEffect(() => {
+      console.log(status);
+      setStatus("pending");
+      console.log(status);
+      fetch(
+        "https://v3.football.api-sports.io/standings?league=39&season=2021",
+        {
+          method: "GET",
+          headers: {
+            "x-rapidapi-host": "v3.football.api-sports.io",
+            "x-rapidapi-key": API_Key,
+          },
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setStatus("resolved");
+          console.log(status);
+          setLeagueTable(data);
+        })
+        .catch((error) => {
+          setStatus("error");
+          console.log(error);
+        });
+      }, []);
+    };
+    LeagueTable();
+    console.log("league table", leagueTable);
 
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h1>Football App</h1>
-        <Main />
+        <Main value={leagueTable}/>
       </header>
     </div>
   );
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-const Main = () => {
+const Main = (props) => {
+    console.log("Main props", props?.value?.parameters?.season);
+    const SeasonYear = props?.value?.parameters?.season;
     return (
         <>
         <h1>MAIN PAGE</h1>
         <h1>Hello, Include NavBar Here</h1>
+        <h1>{SeasonYear}</h1>
         </>
     )
 }
